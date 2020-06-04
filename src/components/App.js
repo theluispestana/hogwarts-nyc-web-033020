4747b2c21c2b9305f35d793b140505f2c6e26788
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-04 08:51:53 -0700

    "working on fourth deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,39 @@ class App extends Component {
     hogs: hogs,
   };
 
+  handleChange = (event) => {
+    event.persist();
+    // console.log(event.target.value)
+    let condition = false;
+    if(event.target.value === 'greased'){
+      condition = true
+    } else if (event.target.value === 'all'){
+      return this.setState({
+        hogs: hogs
+      })
+    }
+    const filteredHogs = hogs.filter(hog => hog.greased === condition)
+    console.log(filteredHogs)
+    this.setState({
+      hogs: filteredHogs
+    })
+  };
+
   render() {
-    return (
+    return (      
       <div className="App">
         <Nav />
+
+        <select onChange={this.handleChange}>
+          <option value='all'>All</option>
+          <option value='greased'>Greased</option>
+          <option value='ungreased'>Ungreased</option>
+        </select>
+
         <HelloWorld />
         <PigContainer hogs={hogs} />
       </div>
+
     );
   }
 }
